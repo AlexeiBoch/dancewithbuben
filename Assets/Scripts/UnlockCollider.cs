734c3dacfd79d7ef56517c2a38cec479d61e18df
vladg <vladgro528@gmail.com>
2024-03-28 01:06:29 +0500

    слелал     -раставил UnlockArea     -собрал правую часть румы на 80%     - 2 из 3 записок с логичиским промыслом     - чек поинты работают

--- Assets/Scripts/UnlockCollider.cs
+++ Assets/Scripts/UnlockCollider.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class UnlockCollider : MonoBehaviour
 {
     [SerializeField] string option;
+    [SerializeField] GameObject TextUnLockAbility;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            TextUnLockAbility.SetActive(true);
             GameManager.UnlockAbility(option);
-            Destroy(gameObject);
+            
         }
+    }
+    public void CloseTextUnLockAbility()
+    {
+        TextUnLockAbility.SetActive(false);
+        Destroy(gameObject);
     }
 }
