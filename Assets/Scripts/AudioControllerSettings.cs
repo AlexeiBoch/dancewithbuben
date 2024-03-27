d9167c445f4ad3aa4213306f4cd3a2fbb8992bad
vladg <vladgro528@gmail.com>
2024-03-27 16:03:25 +0500

    сделал:    -свет

--- Assets/Scripts/AudioControllerSettings.cs
+++ Assets/Scripts/AudioControllerSettings.cs
@@ -9,43 +9,33 @@ public class AudioControllerSettings: MonoBehaviour
     [SerializeField] Sprite audioOff;
     //[SerializeField] GameObject buttonAudio;
 
-    [SerializeField] Slider[] sliders;
-    [SerializeField] AudioClip[] clips;
+    [SerializeField] Slider slider;
 
 
-    // [SerializeField] AudioSource audioSource;
+     [SerializeField] AudioSource audioSource;
 
-    // [SerializeField] AudioClip clip;
-
-    private void Awake()
-    {
-        // Воспроизводим все звуки при старте игры
-        for (int i = 0; i < clips.Length; i++)
-        {
-            AudioSource.PlayClipAtPoint(clips[i], transform.position);
-        }
-    }
+     [SerializeField] AudioClip clip;
 
     private void Update()
     {
-        // Устанавливаем громкость для каждого звука от соответствующего слайдера
-        for (int i = 0; i < sliders.Length; i++)
-        {
-            AudioListener.volume = sliders[i].value;
-        }
+         audioSource.volume = slider.value;
     }
 
-    public void OnOffAudio(int index)
+    public void OnOffAudio()
     {
         if (AudioListener.volume == 1)
         {
             AudioListener.volume = 0;
-            // Установите изображение кнопки audioOff для index-го звука
+            // buttonAudio.GetComponent<Image>().sprite = audioOff;
         }
         else
         {
             AudioListener.volume = 1;
-            // Установите изображение кнопки audioOn для index-го звука
+            //  buttonAudio.GetComponent<Image>().sprite = audioOn;
         }
+    }
+    public void PlaySound()
+    {
+        //audioSource.PlayOneShot(clip);
     }
 }
